c77737496d84ab99bde0d1c79579335bebef0d73
tmkrocks138 <tkhanna@stuy.edu>
2016-12-21 06:38:48 -0500

    START GUI OR SO HELP ME

--- Temp.java
+++ Temp.java
@@ -1,4 +1,11 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
 public class Temp{
+    private Container pane;
+    private JButton toC;
+
     public static double CtoF(double celsius){
 	double fahrenheit = (celsius * 1.8) + 32;
 	return fahrenheit;
@@ -7,12 +14,6 @@ public class Temp{
     public static double FtoC(double fahrenheit){
         double celsius = (fahrenheit - 32) / 1.8;
         return celsius;
-    }
-
-    public static void main(String[]args){
-	System.out.println(CtoF(-40.0));
-	System.out.println(FtoC(212.0));
-
     }
 
 }
