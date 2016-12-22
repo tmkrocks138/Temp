2d5f9b208597e9d251ea2613cf3e59b8dc0c1bfd
Tara Khanna <tmkrocks138@Taras-MacBook-Air.local>
2016-12-22 00:45:27 -0500

    ummmm... this may not work

--- Temp.java
+++ Temp.java
@@ -2,9 +2,51 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class Temp{
+public class Temp extends JFrame implements ActionListener{
     private Container pane;
     private JButton toC;
+    private JButton toC;
+    private JButton toF;
+    private JLabel result;
+
+    public Temp() {
+	this.setTitle("TEMPERATURE CONVERTER");
+	this.setSize(600,400);
+	this.setLocation(100,100);
+	this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+	pane = this.getContentPane();
+	pane.setLayout(new BoxLayout(pane, BoxLayout.Y_AXIS));
+
+	toC = new JButton("toCelsius");
+	toC.addActionListener(this);
+	toC.setActionCommand("findC");
+
+	toF = new JButton("toFahrenheit");
+	toC.addActionListener(this);
+	toC.setActionCommand("findF");
+
+	initial = new JTextField(10);
+
+	pane.add(initial);
+	pane.add(toC);
+	pane.add(toF);
+	
+    }
+
+    public void actionPerformed(ActionEvent e){
+	String event = e.getActionCommand();
+	if(event.equals("findC")){
+	    double ans = TtoC(initial);
+	    result.setText((String)ans);
+	}
+	if(event.equals("findT")){
+	    double ans = CtoT(initial);
+	    result.setText((String)ans);
+	}
+    }
+    
+
 
     public static double CtoF(double celsius){
 	double fahrenheit = (celsius * 1.8) + 32;
